259d132e8f89eec85863b8a202ac257d9eaad60f
Booleano <sergio23.tp@gmail.com>
2025-11-20 10:40:44 +0100

    Ejercicio1 modificado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,8 +1,6 @@
 nombre = input("Introduce tu nombre: ")
 print("¡Hola, " + nombre + "! Encantado de saludarte.")
-
-nombre = input("Introduce tu nombre: ")
 color = input("Introduce tu color favorito: ")
 objeto = input("Introduce un objeto o lugar: ")
 
-print("Hola " + nombre + ", tu color favorito es " + color + " y te gusta el " + objeto + ".")
+print(f"Hola {nombre}, tu color favorito es {color} y te gusta {objeto}.")
